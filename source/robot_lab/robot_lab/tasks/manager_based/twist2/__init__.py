a298332d43cda256c614fff8ffe15df888968c46
Wenqi-Ge <1003662608q@gmail.com>
2026-01-05 00:56:07 +0800

    tmp cmd in twist2

---
+++ source/robot_lab/robot_lab/tasks/manager_based/twist2/__init__.py
@@ -0,0 +1,9 @@
+"""
+@File: __init__.py
+@Author: Winky GE
+@Date: 2026-01-04
+@Brief: move Twist2 in robot_lab
+
+origin repo: https://github.com/amazon-far/TWIST2.git
+"""
+
---
+++ source/robot_lab/robot_lab/tasks/manager_based/twist2/mdp/commands.py
@@ -0,0 +1,80 @@
+"""
+@File: commands.py
+@Author: Winky GE
+@Date: 2026-01-05
+@Brief:  Twist2 environment command definitions
+load motion command from file
+"""
+
+
+
+
+from __future__ import annotations
+
+import math
+import numpy as np
+import os
+import torch
+from collections.abc import Sequence
+from dataclasses import MISSING
+from typing import TYPE_CHECKING
+
+from isaaclab.assets import Articulation
+from isaaclab.managers import CommandTerm, CommandTermCfg
+from isaaclab.markers import VisualizationMarkers, VisualizationMarkersCfg
+from isaaclab.markers.config import FRAME_MARKER_CFG
+from isaaclab.utils import configclass
+from isaaclab.utils.math import (
+    quat_apply,
+    quat_error_magnitude,
+    quat_from_euler_xyz,
+    quat_inv,
+    quat_mul,
+    sample_uniform,
+    yaw_quat,
+)
+from isaaclab.envs import ManagerBasedRLEnv
+
+
+class MotionCommand(CommandTerm):
+    cfg: MotionCommandCfg
+    def __init__(self, cfg: MotionCommandCfg, env: ManagerBasedRLEnv):
+        super().__init__(cfg, env)
+
+        self.robot: Articulation = env.scene[cfg.asset_name]
+        self.robot_anchor_body_index = self.robot.body_names.index(self.cfg.anchor_body_name)
+        self.motion_anchor_body_index = self.cfg.body_names.index(self.cfg.anchor_body_name)
+
+        
+
+
+
+
+
+@configclass
+class MotionCommandCfg(CommandTermCfg):
+    """Configuration for the motion command."""
+
+    class_type: type = MotionCommand
+
+    asset_name: str = MISSING
+
+    motion_file: str = MISSING
+    anchor_body_name: str = MISSING
+    body_names: list[str] = MISSING
+
+    pose_range: dict[str, tuple[float, float]] = {}
+    velocity_range: dict[str, tuple[float, float]] = {}
+
+    joint_position_range: tuple[float, float] = (-0.52, 0.52)
+
+    adaptive_kernel_size: int = 1
+    adaptive_lambda: float = 0.8
+    adaptive_uniform_ratio: float = 0.1
+    adaptive_alpha: float = 0.001
+
+    anchor_visualizer_cfg: VisualizationMarkersCfg = FRAME_MARKER_CFG.replace(prim_path="/Visuals/Command/pose")
+    anchor_visualizer_cfg.markers["frame"].scale = (0.2, 0.2, 0.2)
+
+    body_visualizer_cfg: VisualizationMarkersCfg = FRAME_MARKER_CFG.replace(prim_path="/Visuals/Command/pose")
+    body_visualizer_cfg.markers["frame"].scale = (0.1, 0.1, 0.1)
