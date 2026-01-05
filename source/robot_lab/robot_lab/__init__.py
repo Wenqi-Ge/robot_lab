2a4f8195fe1c0f597642ba170121201cc2b1c92a
Wenqi-Ge <1003662608q@gmail.com>
2026-01-06 00:12:09 +0800

    fit the import lib that could be used in scene that no need open isaacsim

--- source/robot_lab/robot_lab/__init__.py
+++ source/robot_lab/robot_lab/__init__.py
@@ -6,7 +6,23 @@ Python module serving as a project/extension template.
 """
 
 # Register Gym environments.
-from .tasks import *
+# from .tasks import *
+try:
+    import omni.kit.app  # noqa: F401
+except ModuleNotFoundError:
+    # 允许在非 Isaac Sim 环境下 import robot_lab.utils
+    pass
+else:
+    from .tasks import *  # noqa
+
 
 # Register UI extensions.
-from .ui_extension_example import *
+# from .ui_extension_example import *
+
+try:
+    import omni.kit.app  # noqa: F401
+except ModuleNotFoundError:
+    # 允许在非 Isaac Sim 环境下 import robot_lab.utils
+    pass
+else:
+    from .ui_extension_example import *
