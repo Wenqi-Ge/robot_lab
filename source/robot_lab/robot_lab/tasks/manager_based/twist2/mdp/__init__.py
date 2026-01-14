836a476d21bd2c4649fb72172b17486dd4a86b5a
Wenqi-Ge <1003662608q@gmail.com>
2026-01-15 00:46:37 +0800

    add tracking tar

--- source/robot_lab/robot_lab/tasks/manager_based/twist2/mdp/__init__.py
+++ source/robot_lab/robot_lab/tasks/manager_based/twist2/mdp/__init__.py
@@ -7,4 +7,4 @@ from .events import *  # noqa: F401, F403
 from .observations import *  # noqa: F401, F403
 from .rewards import *  # noqa: F401, F403
 from .terminations import *  # noqa: F401, F403
-
+from .assets_name import *  # noqa: F401, F403
